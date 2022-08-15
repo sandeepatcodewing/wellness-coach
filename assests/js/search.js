577af43dcf84f41fa509a16003625af199bbce11
sandeepstha46 <sandeep.codewing@gmail.com>
2022-08-15 11:51:53 +0545

    responsive search bar added

---
+++ assests/js/search.js
@@ -0,0 +1,11 @@
+const searchButton = document.querySelector(".details__icons-navigation--icon");
+const searchBar = document.querySelector(".search");
+const searchClose = document.querySelector(".search__close-icon");
+
+searchButton.addEventListener("click", () => {
+    searchBar.classList.add("search__open");
+});
+
+searchClose.addEventListener("click", () => {
+    searchBar.classList.remove("search__open");
+});
